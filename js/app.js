0c2da560e8da44407269d7e0ff62e86afde99715
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-04-03 17:25:47 +0300

    master

---
+++ js/app.js
@@ -0,0 +1,90 @@
+/* js Элементы */
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true, /* вечная прокрутка */
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    /*
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      */
+      autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        "@0.00": {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        "@0.75": {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        "@1.00": {
+          slidesPerView: 3,
+          spaceBetween: 40,
+        },
+        "@1.50": {
+          slidesPerView: 4,
+          spaceBetween: 50,
+        },
+    },
+  });
+
+
+  $("#myForm").validate({
+    rules: {
+      name: {
+        required: true,
+        minlength: 5
+      }
+    }
+  });
+
+      // вешаем маску на телефон
+      $('.phone-field').inputmask("+7(999)999-9999");
+    
+      // добавляем правило для валидации телефона
+      jQuery.validator.addMethod("checkMaskPhone", function(value, element) {
+          return /\+\d{1}\(\d{3}\)\d{3}-\d{4}/g.test(value); 
+      });
+      
+      // получаем нашу форму по class
+      var form = $('.form-request');
+      
+      // включаем валидацию в форме
+      form.validate();
+      
+      // вешаем валидацию на поле с телефоном по классу
+      $.validator.addClassRules({
+          'phone-field': {
+              checkMaskPhone: true,
+          }
+      });
+      
+      // Проверка на валидность формы при отправке, если нужно
+      form.submit(function(e){
+          e.preventDefault();
+          if (form.valid()) {
+              alert('Форма отправлена');
+          }
+          return;
+      });
+
+
+      document.addEventListener("DOMContentLoaded", function(){
+        /////// Prevent closing from click inside dropdown
+        document.querySelectorAll('.dropdown-menu').forEach(function(element){
+        	element.addEventListener('click', function (e) {
+        		e.stopPropagation();
+        	});
+        })
+    }); 
+	// DOMContentLoaded  end
