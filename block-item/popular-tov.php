77f92f77a03e5b3a4aea05228e1d52fe7381f976
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-04-07 14:39:43 +0300

    master

--- block-item/popular-tov.php
+++ block-item/popular-tov.php
@@ -81,41 +81,13 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="swiper-slide mb-5">
-                            <div class="card b-0 rounded-0">
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide mb-5">
-                            <div class="card b-0 rounded-0">
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
                     </div>
-                    <!--
-                        <div class="swiper-button-next text-white border border-5 border-white" style="background-color: #1A8AAD;"></div>
-                        <div class="swiper-button-prev text-white border border-5 border-white" style="background-color:#BABABA;"></div>
-                        -->
+                    
+                        <div class="swiper-button-next"></div>
+                        <div class="swiper-button-prev"></div>
+                        
                     <div
-                        class="swiper-pagination swiper-pagination-bullets swiper-pagination-horizontal swiper-pagination-bullets-dynamic">
+                        class="swiper-pagination">
                     </div>
                 </div>
             </div>
--- js/app.js
+++ js/app.js
@@ -1,52 +1,38 @@
 /* js Элементы */
 
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    loop: true, /* вечная прокрутка */
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  watchOverflow:true,
+  slidesPerColumn:3,
+  grabCursor:true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    /*
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      */
-      autoplay: {
-        delay: 1500,
-        disableOnInteraction: false,
-      },
-      breakpoints: {
-        "@0.00": {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        "@0.75": {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        "@1.00": {
-          slidesPerView: 3,
-          spaceBetween: 40,
-        },
-        "@1.50": {
-          slidesPerView: 4,
-          spaceBetween: 50,
-        },
+    "@0.75": {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-  });
-
-
-  $("#myForm").validate({
-    rules: {
-      name: {
-        required: true,
-        minlength: 5
-      }
-    }
-  });
+    "@1.00": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  },
+});
 
       // вешаем маску на телефон
       $('.phone-field').inputmask("+7(999)999-9999");
