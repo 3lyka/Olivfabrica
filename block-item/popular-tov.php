3b3bc34063373847b5a07fe0d7f99bad15708ca6
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-04-08 17:33:50 +0300

    master

--- block-item/popular-tov.php
+++ block-item/popular-tov.php
@@ -8,92 +8,32 @@
                     <div class="swiper-wrapper mb-5">
 
                     <?php
-                     get_swiper_all ()
-                     ?>
+                     $swiper = get_swiper_all ();
+                     foreach ($swiper as $swiper): ?>
 
-                        <div class="swiper-slide mb-5">
+                            <div class="swiper-slide mb-5">
                             <div class="card b-0 rounded-0">
-                                <!--Карточка с акцией-->
-                                <p class="m-0 p-2 position-absolute top-0 start-0 text-white b-0"
-                                    style="background-color: #07607C;">17%</p>
-                                <!--Карточка с акцией-->
+                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Кухня "ОК005"</h5>
+                                    <p class="card-text m-0">Стиль: Классический</p>
+                                    <p class="card-text m-0">Цвет: Черный</p>
+                                    <p class="card-text m-0">Материал: ЛДСП</p>
+                                    <a href="#"
+                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
+                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
+                                </div>
+                            </div>
+                        </div>
 
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide mb-5">
-                            <div class="card b-0 rounded-0">
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide mb-5">
-                            <div class="card b-0 rounded-0">
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide mb-5">
-                            <div class="card b-0 rounded-0">
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-slide mb-5">
-                            <div class="card b-0 rounded-0">
-                                <img src="/img/card_tov/card-tovar.png" class="card-img-top p-0 b-0 rounded-0" alt="..." style="height: 14em;">
-                                <div class="card-body">
-                                    <h5 class="card-title">Кухня "ОК005"</h5>
-                                    <p class="card-text m-0">Стиль: Классический</p>
-                                    <p class="card-text m-0">Цвет: Черный</p>
-                                    <p class="card-text m-0">Материал: ЛДСП</p>
-                                    <a href="#"
-                                        class="btn btn-primary btn-danger b-0 rounded-0 mt-3 ms-2 d-flex justify-content-center"
-                                        style="padding: 1% 2% 1% 2%;">от 56 200 ₽ &#8594;</a>
-                                </div>
-                            </div>
-                        </div>
+                    <?php endforeach ?>
+                    
                     </div>
                     
                         <div class="swiper-button-next"></div>
                         <div class="swiper-button-prev"></div>
                         
-                    <div
-                        class="swiper-pagination">
-                    </div>
+                    <div class="swiper-pagination"></div>
                 </div>
             </div>
         </div>
