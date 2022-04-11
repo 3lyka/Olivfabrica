b3bc86300f3206ecaaf56e6be0bcb9217e6b7699
vadim.garaev <68156261+3lyka@users.noreply.github.com>
2022-04-11 13:50:38 +0300

    master

--- catalog-coach.php
+++ catalog-coach.php
@@ -1,112 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/bootstrap.css">
-    <link rel="stylesheet" href="/css/animation.css">
-    <link type="image/x-icon" href="/img/logo/favicon.ico" rel="shortcut icon">
-    <link type="Image/x-icon" href="/img/logo/favicon.ico" rel="icon">
-    <link rel="stylesheet" href="/css/swiper-bundle.css">
-    <link rel="stylesheet" href="/css/style.css">
-    <title>Оливковая ветвь</title>
-</head>
-
+<?php require 'main-parts/head.php'; ?>
 <body>
-    <header class="shadow-sm fixed-top bg-white">
-        <div class="container">
-
-            <!-- ============= COMPONENT ============== -->
-            <nav class="navbar navbar-expand-lg navbar-light rounded-0">
-                <div class="container-fluid">
-                    <div class="good_company d-flex justify-content-start">
-                        <img src="/img/logo/logo.svg" class="img-fluid rounded-0" alt="...">
-                        <p class="mt-3 ms-2 fw-bold" style="color: #127A7B;">Оливковая ветвь</p>
-                    </div>
-                    <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                    <div class="collapse navbar-collapse" id="main_nav">
-                        <ul class="navbar-nav m-0 m-auto">
-                            <li class="nav-item dropdown has-megamenu dropdown-hover">
-                                <a class="nav-link dropdown-toggle fw-bold text-info" href="#"
-                                    data-bs-toggle="dropdown"> Каталог </a>
-                                <div class="dropdown-menu megamenu border-0" role="menu">
-                                    <div class="row g-3">
-                                        <div class="col-lg-3 col-6 p-3">
-                                            <div class="col-megamenu">
-                                                <h6 class="title">Кухни</h6>
-                                                <ul class="list-unstyled lh-lg">
-                                                    <li><a class="text-decoration-none text-dark" href="#">Классический
-                                                            стиль</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Современный
-                                                            стиль</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Хай-тек
-                                                            кухни</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Кухни в стиле
-                                                            модерн</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Скандинавский
-                                                            стиль</a></li>
-                                                </ul>
-                                            </div> <!-- col-megamenu.// -->
-                                        </div><!-- end col-3 -->
-                                        <div class="col-lg-3 col-6 p-3">
-                                            <div class="col-megamenu">
-                                                <h6 class="title">Техника</h6>
-                                                <ul class="list-unstyled lh-lg">
-                                                    <li><a class="text-decoration-none text-dark" href="#">Мойки</a>
-                                                    </li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Смесители</a>
-                                                    </li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Варочные
-                                                            панели</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Вытяжки</a>
-                                                    </li>
-                                                    <li><a class="text-decoration-none text-dark"
-                                                            href="#">Посудомойки</a></li>
-                                                </ul>
-                                            </div> <!-- col-megamenu.// -->
-                                        </div><!-- end col-3 -->
-                                        <div class="col-lg-3 col-6 p-3">
-                                            <div class="col-megamenu">
-                                                <h6 class="title">Шкафы купе</h6>
-                                                <ul class="list-unstyled lh-lg">
-                                                    <li><a class="text-decoration-none text-dark" href="#">Custom
-                                                            Menu</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Custom
-                                                            Menu</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Custom
-                                                            Menu</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Custom
-                                                            Menu</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Custom
-                                                            Menu</a></li>
-                                                    <li><a class="text-decoration-none text-dark" href="#">Custom
-                                                            Menu</a></li>
-                                                </ul>
-                                            </div> <!-- col-megamenu.// -->
-                                        </div><!-- end col-3 -->
-                                    </div><!-- end row -->
-                                </div> <!-- dropdown-mega-menu.// -->
-                            </li>
-                            <li class="nav-item active fw-normal"> <a class="nav-link" href="#">Оплата </a> </li>
-                            <li class="nav-item active fw-normal"> <a class="nav-link" href="#">Акции </a> </li>
-                            <li class="nav-item active fw-normal"> <a class="nav-link" href="#">О компании </a> </li>
-                            <li class="nav-item active fw-normal"> <a class="nav-link" href="#">Контакты </a> </li>
-                            <li class="nav-item active fw-normal"> <a class="nav-link" href="#">Полезная информация </a>
-                            </li>
-                        </ul>
-                    </div> <!-- navbar-collapse.// -->
-                </div>
-                <a class="navbar-brand" href="#"><span class="fw-bold fs-5"> 8 812 317 22 42 </span>
-                    <br><img src="/img/logo/map-pin.svg" alt="" class="p-0 m-0"><span class="fs-6 ms-1">Санкт Петербург</span></a>
-                <!-- container-fluid.// -->
-            </nav>
-
-        </div>
-    </header>
+<?php require 'main-parts/header.php'; ?>
 
     <main style="margin-top:180px;">
         <div class="container">
@@ -419,68 +316,10 @@
         </div>
         </div>
     </main>
-    <div style="background-color: #07607C;">
-        <footer class="mt-5 mb-0">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-6">
-                        <ul class="list-unstyled mt-5 mb-5">
-                            <li><a href="#" class="nav-link text-white">Оплата</a></li>
-                            <li><a href="#" class="nav-link text-white">Акции</a></li>
-                            <li><a href="#" class="nav-link text-white">О компании</a></li>
-                            <li><a href="#" class="nav-link text-white">Контакты</a></li>
-                            <li><a href="#" class="nav-link text-white">Полезная информация</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <ul class="list-unstyled mt-5 mb-5">
-                            <li><a href="#" class="nav-link text-white">Кухни</a></li>
-                            <li><a href="#" class="nav-link text-white">Техника</a></li>
-                            <li><a href="#" class="nav-link text-white">Шкафы купе</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <ul class="list-unstyled mt-5 mb-5">
-                            <li class="text-white">Юридический и фактический <br> адрес: 194100 г. Санкт-Петербург , <br> Лесной проспект д. 63 лит А, кабинет 410
-                            </li> <br>
-                            <li class="text-white">
-                                ИНН/КПП: 7802886450/780201001 <br>
-                                ОГРН: 1207800162096 <br>
-                                Банк: АО «Тинькофф»
-                                БИК: 044525974</li>
-                        </ul>
-                    </div>
-                    <div class="col-md-3 col-6">
-                        <ul class="list-unstyled mt-5 mb-5">
-                            <li><a href="#" class="nav-link text-white"></a>
-                                <button type="button" class="btn text-white rounded-0 bg-info mb-2 p-2" data-bs-toggle="modal" data-bs-target="#exampleModal">Оставить заявку</button>
-                            </li>
-                            <li><a href="#"
-                                    class="nav-link text-white"><b> <svg class="mx-2" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                                width="16" height="16"
-                                viewBox="0 0 16 16"
-                                style=" fill:#ffffff;"><path d="M 7.5 1 C 3.917969 1 1 3.917969 1 7.5 C 1 11.082031 3.917969 14 7.5 14 C 11.082031 14 14 11.082031 14 7.5 C 14 3.917969 11.082031 1 7.5 1 Z M 7.5 2 C 10.542969 2 13 4.457031 13 7.5 C 13 10.542969 10.542969 13 7.5 13 C 4.457031 13 2 10.542969 2 7.5 C 2 4.457031 4.457031 2 7.5 2 Z M 7 3 L 7 8 L 10 8 L 10 7 L 8 7 L 8 3 Z"></path></svg> 9:00-21:00</b></a>
-                            </li>
-                            <li><a href="#"
-                                    class="nav-link text-white"><b> <img src="icon/phone.svg" alt="" class="mx-2"> 8 812 317 22 42</b></a>
-                            </li>
-                            <li><a href="#"
-                                    class="nav-link text-white"><b> <img src="icon/mail.svg" alt="" class="mx-2"> info@olivfabrika.ru</b></a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="text-white ms-3 p-3">
-                <script>
-                    function y2k(number) { return (number < 1000) ? number + 1900 : number; }
-                    var today = new Date();
-                    var year = y2k(today.getYear());
-                    document.write('Digital Agency IMOS © '+year+' Devoloper Гараев Вадим');
-                </script>
-            </div>
-        </footer>
-    </div>
+
+
+    
+    <?php require 'main-parts/footer.php'; ?>
 
 
     <!--
